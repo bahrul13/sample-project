9e57a788f437cb7c0b1bb6e904a1165eab0071b6
bahrul13 <ungadbahrul94@gmail.com>
2022-10-08 14:22:33 +0800

    added third Fragment

--- app/src/main/java/com/example/sampleproject/ThirdFragment.kt
+++ app/src/main/java/com/example/sampleproject/ThirdFragment.kt
@@ -1,19 +1,17 @@
 package com.example.sampleproject
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.sampleproject.databinding.FragmentFirstBinding
+import com.example.sampleproject.databinding.FragmentSecondBinding
+import com.example.sampleproject.databinding.FragmentThirdBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
+class ThirdFragment: Fragment(){
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentThirdBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +22,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentThirdBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,9 +30,8 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-           findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-//                   binding.textviewFirst.text = "Hello World!"
+        binding.buttonThird.setOnClickListener {
+            findNavController().navigate(R.id.action_ThirdFragment_to_FirstFragment)
         }
     }
 
